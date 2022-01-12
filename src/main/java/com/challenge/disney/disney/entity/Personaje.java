36a33847afc6bc66d38e54887f90a3e8473b0f67
“Camila <camilaguz54@gmail.com>
2022-01-12 12:52:34 -0300

    Creacion de entidad

---
+++ src/main/java/com/challenge/disney/disney/entity/Personaje.java
@@ -0,0 +1,30 @@
+package com.challenge.disney.disney.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "Personaje")
+@Getter
+@Setter
+public class Personaje {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String imagen;
+
+    private String nombre;
+
+    private int edad;
+
+    private double peso;
+
+    private String historia;
+
+
+
+}
